a52d69e1de4787a0d8ec198eee46ff008bf8fd5a
Anujaa1 <acharyaanujaa@gmail.com>
2026-01-03 19:50:44 +0545

    Fix mistakes in DSA programs

--- question_15.c
+++ question_15.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-void search(int a[], int n, int x) {
-    for(int i = 0; i < n; i++)
-        if(a[i] == x)
-            printf("%d ", i);
+void searchAll(int arr[], int size, int key) {
+    int found = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            printf("Found at index %d\n", i);
+            found = 1;
+        }
+    }
+    if (!found)
+        printf("Element not found.\n");
 }
 
 int main() {
-    int a[10], n, x;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
-    scanf("%d", &x);
-    search(a, n, x);
+    int size, key;
+    printf("Enter size: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter elements: ");
+    for (int i = 0; i < size; i++) scanf("%d", &arr[i]);
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    searchAll(arr, size, key);
     return 0;
 }
